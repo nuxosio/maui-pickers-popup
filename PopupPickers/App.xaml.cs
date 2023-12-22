f04abaf9187918949eef2558e51cb3250e00a6a2
Jorge Nuricumbo <Jorge.Nuricumbo@nuxos.io>
2023-12-22 21:26:56 +0100

    Initial commit

---
+++ PopupPickers/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PopupPickers
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PopupPickers/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PopupPickers
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
